7fbf499577e85c93c5406e551e32b7d032ae4743
Louis Suo <louis.suo@myob.com>
2016-07-29 11:38:27 +1000

    extract localauth and ec2auth

--- routes/token.js
+++ routes/token.js
@@ -1,32 +1,36 @@
 var express = require('express');
+var _ = require('lodash');
 var router = express.Router();
 
 var AWS = require('aws-sdk');
 
-AWS.config.credentials = new AWS.EC2MetadataCredentials();
+function ec2Auth() {
+  return new AWS.EC2MetadataCredentials();
+}
 
+function localAuth() {
+  AWS.config.credentials = new AWS.EnvironmentCredentials('AWS');
 
-AWS.config.credentials = new AWS.TemporaryCredentials({
-  RoleArn: 'arn:aws:iam::676890035424:role/ec2-kinesis',
-  RoleSessionName: 'RoleSessionName1',
-  expireTime: new Date()
-});
+  AWS.config.credentials = new AWS.TemporaryCredentials({
+    RoleArn: 'arn:aws:iam::676890035424:role/delegate',
+    RoleSessionName: 'RoleSessionName1',
+    expireTime: new Date()
+  });
+  return AWS.config.credentials;
+}
 
-AWS.config.credentials = new AWS.TemporaryCredentials({
-  RoleArn: 'arn:aws:iam::676890035424:role/delegate',
-  RoleSessionName: 'RoleSessionName2',
-  expireTime: new Date()
-});
+// AWS.config.credentials = localAuth();
+AWS.config.credentials = ec2Auth();
+
+AWS.config.credentials.refresh();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   console.log(AWS.config.credentials);
   AWS.config.credentials.refresh(function () {
-    res.send({
-      accessKeyId: AWS.config.credentials.accessKeyId,
-      secretAccessKey: AWS.config.credentials.secretAccessKey,
-      sessionToken: AWS.config.credentials.sessionToken
-    });
+    res.send(
+      _.pick(AWS.config.credentials, ['accessKeyId', 'secretAccessKey', 'sessionToken', 'expireTime'])
+      );
   })
 
 });
